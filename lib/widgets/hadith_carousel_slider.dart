ea1fee6a171b27186df1ac805d50a5225f24d69e
Ahmed Walied <2301647@student.eelu.edu.eg>
2026-06-27 03:56:16 +0300

    refactor: rename hadith_carousel_slider file

--- lib/widgets/hadith_carousel_slider.dart
+++ lib/widgets/hadith_carousel_slider.dart
@@ -2,8 +2,8 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:islami_app/widgets/body_hadith_view.dart';
 
-class HadithListView extends StatelessWidget {
-  const HadithListView({super.key});
+class HadithCarouselSlider extends StatelessWidget {
+  const HadithCarouselSlider({super.key});
 
   @override
   Widget build(BuildContext context) {
